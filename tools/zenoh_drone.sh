c06c821928389273005952585cf97bb1345c97d8
Harvey Merton <hmer101@mit.edu>
2025-05-26 22:30:52 +0000

    Zenoh works with drone 1 and laptop. properly isolated!

--- tools/zenoh_drone.sh
+++ tools/zenoh_drone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run zenoh bridge
-ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /env_drone_dev/ws_ros2/src/zenoh_vendor/configs/zenoh_gcs.json5
+ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /home/ws_ros2/src/zenoh_vendor/configs/zenoh_drone.json5
--- tools/zenoh_gcs.sh
+++ tools/zenoh_gcs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run zenoh bridge
-ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /env_drone_dev/ws_ros2/src/zenoh_vendor/configs/zenoh_gcs.json5
+ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /multi_drone_slung_load_master/ws_ros2/src/zenoh_vendor/configs/zenoh_gcs.json5
